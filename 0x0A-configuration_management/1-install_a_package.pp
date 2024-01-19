439e797e6f8793e386ee055fda7b17b785dbfde1
Hatim Selmun <selmun13@gmail.com>
2024-01-19 12:20:37 +0100

    fixing flask installation

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,6 @@
 # Installing flask from pip3
 
-exec { 'apt-get update':
-    command => '/bin/apt-get update',
-}
-
 package { 'flask':
     ensure   => 'installed',
-    require  => Exec['apt-get update'],
     provider => pip3,
 }
