f5641c5e475904aac5b5cb59513ef7a00ea255e2
Hatim Selmun <selmun13@gmail.com>
2024-02-19 17:07:02 +0100

    tocsv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Gather data from an API
+then export to CSV
+"""
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        prin("Usage: ./file_name id")
+    id = sys.argv[1]
+    url = f"https://jsonplaceholder.typicode.com/users/{id}"
+    req = requests.get(url).json()
+    tasks_url = url + "/todos"
+    tasks = requests.get(tasks_url).json()
+
+    completed_tasks = []
+    for task in tasks:
+        if task.get('completed'):
+            completed_tasks.append(task.get('title'))
+
+    field_names = ['USER_ID', 'USERNAME',
+                   'TASK_COMPLETED_STATUS',
+                   'TASK_TITLE']
+
+    dictt = {'USER_ID': str(req.get('id')), 'USERNAME': str(req.get('name'))}
+
+    file_name = str(dictt['USER_ID']) + '.csv'
+    with open(file_name, 'w') as f:
+        writer = csv.DictWriter(f, fieldnames=field_names)
+
+        writer.writeheader()
+        for task in tasks:
+            dictt['TASK_COMPLETED_STATUS'] = str(task.get('completed'))
+            dictt['TASK_TITLE'] = str(task.get('title'))
+            writer.writerow(dictt)
