a4a654d960bc611861e6031e65ecabb77ad729be
Hatim Selmun <selmun13@gmail.com>
2024-02-19 12:10:41 +0100

    users and taks

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,20 +6,21 @@ import requests
 import sys
 
 
-if len(sys.argv) < 2:
-    prin("Usage: ./file_name id")
-id = sys.argv[1]
-url = f"https://jsonplaceholder.typicode.com/users/{id}"
-req = requests.get(url).json()
-tasks_url = url + "/todos"
-tasks = requests.get(tasks_url).json()
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        prin("Usage: ./file_name id")
+    id = sys.argv[1]
+    url = f"https://jsonplaceholder.typicode.com/users/{id}"
+    req = requests.get(url).json()
+    tasks_url = url + "/todos"
+    tasks = requests.get(tasks_url).json()
 
-completed_tasks = []
-for task in tasks:
-    if task['completed']:
-        completed_tasks.append(task['title'])
+    completed_tasks = []
+    for task in tasks:
+        if task.get('completed'):
+            completed_tasks.append(task.get('title'))
 
-print("Employee {} is done with tasks({}/{}):"
-      .format(req['name'], len(completed_tasks), len(tasks)))
-for task in completed_tasks:
-    print("\t {}".format(task))
+    print("Employee {} is done with tasks({}/{}):"
+        .format(req['name'], len(completed_tasks), len(tasks)))
+    for task in completed_tasks:
+        print("\t {}".format(task))
