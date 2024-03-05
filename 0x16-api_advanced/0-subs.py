b636aa34232ec4e8637a7a417c9a8f13e3bbee5c
Hatim Selmun <selmun13@gmail.com>
2024-03-05 19:50:42 +0100

    number of subscribes

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Requests reddit api
+"""
+import requests
+import sys
+
+
+def number_of_subscribers(subreddit):
+    """
+    Takes one argument: subreddit
+    Returns the number of subscribers
+    """
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64)\
+                Chrome/119.0.0.0 Safari/537.36 (by/u/Hatim)"}
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    req = requests.get(url, allow_redirects=False, headers=headers)
+    if not req:
+        return 0
+    js = req.json()
+    js_data = js['data']
+    subscribers = js_data['subscribers']
+    return subscribers
