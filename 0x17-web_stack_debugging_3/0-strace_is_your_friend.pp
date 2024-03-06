f8fee57f6169b90121e1f9cf3d57d0a65ea2f8ca
Hatim Selmun <selmun13@gmail.com>
2024-03-06 19:56:05 +0100

    fixing apache 500 error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
 # Resolve 500 error triggered by GET HTTP method request to Apache web server
 
-exec { 'fix_apache_500_error':
-  provider => 'shell',
-  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
-  notify   => Service['apache'],
+exec {'correcting_typo':
+  provider => shell,
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
