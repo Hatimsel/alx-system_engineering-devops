885fca696610e6eedf307641d037e936d19830c8
Hatim Sel <selmun13@gmail.com>
2024-03-14 14:41:52 +0000

    change os config

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,10 @@
 # Change the OS configuration
-exec { 'Change_OS_configuration':
-  command  => 'sudo sed -i \'s/nofile 5/nofile 30000/\' /etc/security/limits.conf; sudo sed -i \'s/nofile 4/nofile 10000/\' /etc/security/limits.conf',
+
+exec { 'change_hard':
+  command  => 'sudo sed -i \'s/nofile 5/nofile 30000/\' /etc/security/limits.conf',
+  provider => shell,
+}
+exec { 'change_soft':
+  command  => 'sudo sed -i \'s/nofile 4/nofile 10000/\' /etc/security/limits.conf',
   provider => shell,
 }
